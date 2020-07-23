de5733f66c3dc25ab1202358794eb83d05da12f8
Santiago <tiagosuarez22@gmail.com>
2020-07-23 16:06:15 -0500

    Cambio en animacion de olas login: farol añadido

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ app.listen(port, (err)=>{
     if(err){
         return console.log(err);
     }
-    // mongodb://localhost:27017/db_navegantes
-    mongoose.connect('mongodb://navegantes:navegantes_1@ds133670.mlab.com:33670/db_navegantes', {useNewUrlParser:true, useUnifiedTopology:true}, (errConn)=>{
+    let local='mongodb://localhost:27017/db_navegantes';
+    let web='mongodb://navegantes:navegantes_1@ds133670.mlab.com:33670/db_navegantes';
+    mongoose.connect(local, {useNewUrlParser:true, useUnifiedTopology:true}, (errConn)=>{
         if(errConn){
             return console.log(errConn);
         }
